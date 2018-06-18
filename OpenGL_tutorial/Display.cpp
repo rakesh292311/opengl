bd6ab5d0cd2fe032f50fd0c32dd16fd545dfb4e8
rakesh292311 <rakesh.carey@gmail.com>
2018-06-18 10:53:06 +0530

    final

--- OpenGL_tutorial/Display.cpp
+++ OpenGL_tutorial/Display.cpp
@@ -1,47 +1,48 @@
-#include "Display.h"
+#include "display.h"
+#include <GL/glew.h>
 #include <iostream>
-#define GLEW_STATIC
-#include <GL\glew.h>
-#include <iostream>
-#include <string>
-using namespace std;
-Display::Display(int width, int height, const string& title)
+
+Display::Display(int width, int height, const std::string& title)
 {
 	SDL_Init(SDL_INIT_EVERYTHING);
+
 	SDL_GL_SetAttribute(SDL_GL_RED_SIZE, 8);
 	SDL_GL_SetAttribute(SDL_GL_GREEN_SIZE, 8);
 	SDL_GL_SetAttribute(SDL_GL_BLUE_SIZE, 8);
 	SDL_GL_SetAttribute(SDL_GL_ALPHA_SIZE, 8);
 	SDL_GL_SetAttribute(SDL_GL_BUFFER_SIZE, 32);
+	SDL_GL_SetAttribute(SDL_GL_DEPTH_SIZE, 16);
 	SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
+
 	m_window = SDL_CreateWindow(title.c_str(), SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, width, height, SDL_WINDOW_OPENGL);
-	m_glcontext = SDL_GL_CreateContext(m_window);
-	GLenum status = glewInit();
-	if (status != GLEW_OK)
-		cout << "glew failed" << endl;
-	m_isClosed = false;
+	m_glContext = SDL_GL_CreateContext(m_window);
+
+	GLenum res = glewInit();
+	if (res != GLEW_OK)
+	{
+		std::cerr << "Glew failed to initialize!" << std::endl;
+	}
+
+	glEnable(GL_DEPTH_TEST);
+
+	glEnable(GL_CULL_FACE);
+	glCullFace(GL_BACK);
 }
 
-bool Display::IsClosed()
-{
-	return m_isClosed;
-}
 Display::~Display()
 {
-	SDL_GL_DeleteContext(m_glcontext);
+	SDL_GL_DeleteContext(m_glContext);
 	SDL_DestroyWindow(m_window);
 	SDL_Quit();
 }
 
-void Display::Update()
+void Display::Clear(float r, float g, float b, float a)
+{
+	glClearColor(r, g, b, a);
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+}
+
+void Display::SwapBuffers()
 {
 	SDL_GL_SwapWindow(m_window);
-	
-	SDL_Event e;
-
-	while (SDL_PollEvent(&e))
-	{
-		if (e.type == SDL_QUIT)
-			m_isClosed = true;
-	}
 }
--- OpenGL_tutorial/Display.h
+++ OpenGL_tutorial/Display.h
@@ -1,21 +1,25 @@
-#pragma once
+#ifndef DISPLAY_INCLUDED_H
+#define DISPLAY_INCLUDED_H
+
 #include <string>
-#include <SDL2\SDL.h>
+#include <SDL2/SDL.h>
 
 class Display
 {
 public:
 	Display(int width, int height, const std::string& title);
+
+	void Clear(float r, float g, float b, float a);
+	void SwapBuffers();
+
 	virtual ~Display();
-	void Update();
-	bool IsClosed();
+protected:
 private:
-	Display(const Display & other) {}
-	Display & operator=(const Display& other) {}
+	void operator=(const Display& display) {}
+	Display(const Display& display) {}
 
-	SDL_Window *m_window;
-	SDL_GLContext m_glcontext;
-	bool m_isClosed;
-
+	SDL_Window* m_window;
+	SDL_GLContext m_glContext;
 };
 
+#endif
